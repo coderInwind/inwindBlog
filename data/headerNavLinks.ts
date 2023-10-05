86bcb76bad27bdb07d88955190b99de16175f0c7
liuji <1565692947@qq.com>
2023-10-05 15:40:52 +0800

    10.5

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,5 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
+  { href: '/', title: '主页' },
   { href: '/blog', title: '博客' },
   { href: '/tags', title: '归档' },
   { href: '/projects', title: '项目' },
